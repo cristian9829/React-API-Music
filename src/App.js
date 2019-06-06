ddb3c6bfe9fe5ec17063251810f06078d3e3edd6
Cristian Clavijo <cristian982940@gmail.com>
2019-06-05 23:23:19 -0500

    show results of the letra

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, {useState, useEffect, Fragment} from 'react';
 import Formulario from './components/Formulario';
+import Cancion from './components/Cancion';
+import axios from 'axios';
 
 function App (){
 
@@ -9,14 +11,35 @@ function App (){
   const [info, agregarInfo] = useState({});
 
   // Metodo para consultar la api de letras de canciones
-  const consultarApiLetra = busqueda =>{
-    console.log(busqueda)
+  const consultarApiLetra = async busqueda =>{
+    const {artista, cancion} = busqueda;
+    const url = `https://api.lyrics.ovh/v1/${artista}/${cancion}`;
+
+    //consultar la API
+    const resultado = await axios(url)
+    
+    //almacenar la letra en el state
+    agregarLetra(resultado.data.lyrics)
+    console.log(letra)
   }
   return(
     <Fragment>
       <Formulario
         consultarApiLetra = {consultarApiLetra}
       /> 
+
+      <div className="container mt-5">
+        <div className="row">
+          <div className="col-md-6">
+
+          </div>
+          <div className="col-md-6">
+            <Cancion
+              letra = {letra}
+            />
+          </div>
+        </div>
+      </div>
     </Fragment>
   )
 }
