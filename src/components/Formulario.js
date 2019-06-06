441fc3549c01c961e44205632e09ff73d20771db
Cristian Clavijo <cristian982940@gmail.com>
2019-06-05 22:56:56 -0500

    read values form in states save

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,46 +1,64 @@
 import React, {useState} from 'react';
 
 function Formulario (){
+   
+   const [busqueda, agregarBusqueda] = useState({
+     artista: '',
+     cancion: ''
+   })
+
+   //Funcion para actualizar el state de los inputs
+   const actualizarState = e =>{
+     agregarBusqueda({
+       ...busqueda,
+       [e.target.name] : e.target.value
+     })
+
+     console.log(busqueda)
+   }
+
   return(
     <div className="bg-info">
-          <div className="container">
+      <div className="container">
+        <div className="row">
+          <form 
+            className="col card text-white bg-transparent  mb-5 pt-5 pb-2">
+            <fieldset>
+              <legend className="text-center">Buscador Letras Canciones</legend>
               <div className="row">
-                  <form 
-                    className="col card text-white bg-transparent  mb-5 pt-5 pb-2">
-                      <fieldset>
-                          <legend className="text-center">Buscador Letras Canciones</legend>
-                          <div className="row">
-                              <div className="col-md-6">
-                                <div className="form-group">
-                                    <label>Artista</label>
-                                    <input 
-                                        type="text" 
-                                        className="form-control" 
-                                        name="artista" 
-                                        placeholder="Nombre Artista" 
-                                        required
-                                    />
-                                </div>
-                              </div>
-                              <div className="col-md-6">
-                                <div className="form-group">
-                                    <label>Canción</label>
-                                    <input 
-                                        type="text" 
-                                        className="form-control" 
-                                        name="cancion" 
-                                        placeholder="Nombre Canción" 
-                                        required
-                                    />
-                                </div>
-                              </div>
-                          </div>
-                          <button type="submit" className="btn btn-primary float-right">Buscar</button>
-                      </fieldset>
-                  </form>
+                <div className="col-md-6">
+                  <div className="form-group">
+                    <label>Artista</label>
+                    <input 
+                      type="text" 
+                      className="form-control" 
+                      name="artista" 
+                      placeholder="Nombre Artista" 
+                      onChange={actualizarState}
+                      required
+                    />
+                  </div>
+                </div>
+                <div className="col-md-6">
+                  <div className="form-group">
+                    <label>Canción</label>
+                    <input 
+                      type="text" 
+                      className="form-control" 
+                      name="cancion" 
+                      placeholder="Nombre Canción" 
+                      onChange={actualizarState}
+                      required
+                    />
+                  </div>
+                </div>
               </div>
-          </div>
+              <button type="submit" className="btn btn-primary float-right">Buscar</button>
+            </fieldset>
+          </form>
+        </div>
       </div>
+    </div>
   )
 }
 
